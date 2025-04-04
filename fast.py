6334cd9243b47cb317970cf1ef505afc955f93f5
Chinmay Mokashi <cmokashi@mdanderson.org>
2025-04-04 16:03:25 -0500

    Remove static from fast api script

--- fast.py
+++ fast.py
@@ -1,6 +1,9 @@
-from lib.api.process import image, exec
-from lib.api import pipeline
-from lib.api.utils import log_reader
+# from lib.api.process import image, exec
+# from lib.api import pipeline
+# from lib.api.utils import log_reader
+from app.routes.process import image, exec
+from app.routes import pipeline
+from app.routes.utils import log_reader
 
 import asyncio
 from pathlib import Path
@@ -17,7 +20,7 @@ app.include_router(pipeline.router, prefix="/pipeline", tags=["pipeline"])
 app.include_router(image.router, prefix="/process/image", tags=["process", "image"])
 app.include_router(exec.router, prefix="/process/exec", tags=["process", "exec"])
 
-app.mount("/static", StaticFiles(directory="logs/templates/", html=True), name="static")
+# app.mount("/static", StaticFiles(directory="logs/templates/", html=True), name="static")
 
 app.add_middleware(
     CORSMiddleware,
@@ -31,17 +34,17 @@ app.add_middleware(
 async def root():
     return {"message": "Hello World"}
 
-@app.websocket("/ws/log")
-async def stream(websocket: WebSocket, ):
-    await websocket.accept()
-    try:
-        while True:
-            await asyncio.sleep(1)
-            logs = await log_reader("logs/app.log")
-            await websocket.send_text("".join(logs))
-    except Exception as e:
-        await websocket.close()
-        print(f"Websocket closed. Error: {e}")
-    finally:
-        await websocket.close()
-        print("Websocket closed.")
+# @app.websocket("/ws/log")
+# async def stream(websocket: WebSocket, ):
+#     await websocket.accept()
+#     try:
+#         while True:
+#             await asyncio.sleep(1)
+#             logs = await log_reader("logs/app.log")
+#             await websocket.send_text("".join(logs))
+#     except Exception as e:
+#         await websocket.close()
+#         print(f"Websocket closed. Error: {e}")
+#     finally:
+#         await websocket.close()
+#         print("Websocket closed.")
