27d607c29d3784198fd49f15934943069d98a76f
Mokashi,Chinmay <CMokashi@mdanderson.org>
2025-04-11 22:36:09 -0500

    Allow arbitrary types

--- app/models/insight/connection.py
+++ app/models/insight/connection.py
@@ -1,12 +1,14 @@
 from ...utils.constants import *
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 from pydantic_ai import Agent
 from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai.providers.openai import OpenAIProvider
 from openai import AsyncOpenAI
 
 class LLMConnection(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    
     client: AsyncOpenAI
     provider: OpenAIProvider
     model: OpenAIModel
