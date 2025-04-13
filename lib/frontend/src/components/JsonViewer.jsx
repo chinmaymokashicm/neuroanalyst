7eea6bfcc712a95656fa0065dd861717c733e664
Mokashi,Chinmay <CMokashi@mdanderson.org>
2025-04-13 14:39:31 -0500

    Remove lib/

--- lib/frontend/src/components/JsonViewer.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactJson from 'react-json-view';
-
-const JsonViewer = ({ data }) => {
-  return <ReactJson src={data} theme="monokai" collapsed={2} />;
-};
-
-export default JsonViewer;
--- lib/utils/constants.py
+++
@@ -1,8 +0,0 @@
-MONGO_URI: str = "mongodb://localhost:27017/"
-DB_NAME: str = "neuroanalyst"
-
-# DB Collections
-COLLECTION_PROCESS_IMAGES: str = "process_images"
-COLLECTION_PROCESS_EXECS: str = "process_execs"
-COLLECTION_PIPELINES: str = "pipelines"
-COLLECTION_SUMMARIES: str = "summaries"
