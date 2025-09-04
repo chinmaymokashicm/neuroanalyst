2be63d586e98278e6ce9e381450ba6f8640cbc55
Mokashi,Chinmay <CMokashi@mdanderson.org>
2025-09-04 15:37:19 -0500

    Replace setup.sh with set_envs.sh

--- src/neuroanalyst/scripts/setup.py
+++ src/neuroanalyst/scripts/setup.py
@@ -3,5 +3,5 @@ import pathlib
 
 def main():
     root = pathlib.Path(__file__).resolve().parent.parent
-    script = root / "setup.sh"
+    script = root / "set_envs.sh"
     subprocess.run(["bash", str(script)], check=True)
