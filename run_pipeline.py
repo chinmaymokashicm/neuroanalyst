c66823821cd542c15e40c4d7b2d2f01cfef82bd2
Mokashi,Chinmay <CMokashi@mdanderson.org>
2025-10-08 12:05:17 -0500

    Add file logging and remove job name directives in templates
    Added a file handler to the logger in run_pipeline.py to write logs to a pipeline-specific file. Removed job name directives from LSF, PBS, and SLURM template scripts to simplify job submission headers.

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,4 +1,5 @@
-from neuroanalyst.models.pipeline import NeuPipeline
+from src.neuroanalyst.models.pipeline import NeuPipeline
+from src.neuroanalyst.utils import NeuroAnalystPaths
 
 import logging, sys
 
@@ -7,6 +8,17 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 pipeline_id: str = str(sys.argv[1])
+
+# Set up file handler for logging
+paths = NeuroAnalystPaths()
+logs_dir = paths.logs
+
+log_file_path = f"{logs_dir}/pipelines/{pipeline_id}/{pipeline_id}.log"
+file_handler = logging.FileHandler(log_file_path, mode='a')
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 pipeline: NeuPipeline = NeuPipeline.from_pipeline_id(pipeline_id)
 
