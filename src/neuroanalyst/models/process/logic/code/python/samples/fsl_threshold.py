78ea63a1fe751eef0c54ff33b9b001bfbcc4d7d6
Mokashi,Chinmay <CMokashi@mdanderson.org>
2025-10-13 19:23:01 -0500

    Fix threshold type and remove debug prints in fsl_threshold
    Changed the threshold variable from int to float to correctly handle decimal values and removed unnecessary print statements for cleaner output.

--- src/neuroanalyst/models/process/logic/code/python/samples/fsl_threshold.py
+++ src/neuroanalyst/models/process/logic/code/python/samples/fsl_threshold.py
@@ -25,10 +25,8 @@ def fsl_threshold(input_filepath: str):
         output_entities (dict): Dictionary of BIDS entities for the output file.
         forced_outputs (list): List of file paths that are saved as outputs but not BIDS-compliant.
     """
-    print(f"Input file path: {input_filepath}")
     # Step 1: Prepare environment and paths
-    threshold: int = int(os.getenv("THRESHOLD", "0.5"))
-    print(f"Using threshold value: {threshold}")
+    threshold: float = float(os.getenv("THRESHOLD", "0.5"))
     
     DATA_DIR = "/data"  # shared data dir bind
     fsl_img_name = os.getenv("FSL_IMG_NAME")
