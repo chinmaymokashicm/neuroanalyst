a27cfe962490f3696048337d5b47fb3868df8c14
Mokashi,Chinmay <CMokashi@mdanderson.org>
2025-10-21 01:25:23 -0500

    Update process.py

--- src/neuroanalyst/routes/process.py
+++ src/neuroanalyst/routes/process.py
@@ -31,7 +31,6 @@ class ProcessDirRequest(BaseModel):
     logic_name: Optional[str] = Field(None, description="ID of the NeuProcessLogic to use")
     config: Optional[NeuProcessDirConfig | dict] = Field(None, description="Configuration for the NeuProcessDir")
     generate: bool = Field(False, description="Whether to generate the process directory after creation")
-    overwrite: bool = Field(False, description="Whether to overwrite existing process directory if it exists")
     
 class ProcessRequest(BaseModel):
     """
@@ -75,10 +74,6 @@ async def create_process_dir(request: ProcessDirRequest):
         process_dir: NeuProcessDir = NeuProcessDir.from_logic(logic, config=config)
         
         if request.generate:
-            if request.overwrite:
-                process_dir_path: Path = PATHS.workdir / process_dir.process_id
-                if process_dir_path.exists():
-                    shutil.rmtree(process_dir_path)
             process_dir.generate()
         
         return {
