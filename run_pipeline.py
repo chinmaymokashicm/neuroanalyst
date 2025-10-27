28d0099451429014172458c1a4d1ae8c8f73cfda
Mokashi,Chinmay <CMokashi@mdanderson.org>
2025-10-27 15:08:06 -0500

    Remove logging setup from run_pipeline.py and simplify execution call

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,29 +1,9 @@
 from src.neuroanalyst.models.pipeline import NeuPipeline
-from src.neuroanalyst.utils import NeuroAnalystPaths
 
-import logging, sys, os
-
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+import sys
 
 pipeline_id: str = str(sys.argv[1])
-
-# Set up file handler for logging
-paths = NeuroAnalystPaths()
-logs_dir = paths.logs
-
-log_file_path = f"{logs_dir}/pipelines/{pipeline_id}/{pipeline_id}.log"
-# Create the log directory if it doesn't exist
-os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
-
-file_handler = logging.FileHandler(log_file_path, mode='a')
-file_handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
 pipeline: NeuPipeline = NeuPipeline.from_pipeline_id(pipeline_id)
 
 if __name__ == "__main__":
-    result: str = pipeline.execute_via_python(logger=logger, resume=True)
+    result: str = pipeline.execute_via_python(resume=True)
