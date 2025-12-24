c7ccca60a2ddc136227426e7f9a0675ce835df46
Mokashi,Chinmay <CMokashi@mdanderson.org>
2025-12-23 22:42:02 -0600

    Add username argument to pipeline reset script
    The script now accepts a username as a second command-line argument and passes it to NeuPipeline.from_pipeline_id. This allows resetting pipeline status in a user-specific context.

--- reset_pipeline_status.py
+++ reset_pipeline_status.py
@@ -3,6 +3,7 @@ import sys
 from src.neuroanalyst.models.pipeline import NeuPipeline
 
 pipeline_id: str = str(sys.argv[1])
+username: str = str(sys.argv[2])
 
-pipeline: NeuPipeline = NeuPipeline.from_pipeline_id(pipeline_id)
+pipeline: NeuPipeline = NeuPipeline.from_pipeline_id(pipeline_id, username=username)
 pipeline.reset_pipeline_status()
