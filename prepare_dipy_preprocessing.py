e6eb48cc3c12d54721d354cce172b84aecdf2110
Mokashi,Chinmay <CMokashi@mdanderson.org>
2025-12-30 19:59:38 -0600

    Add scripts for DIPY and FreeSurfer pipeline preparation
    Introduces prepare_dipy_preprocessing.py for building a DIPY-based DWI preprocessing pipeline and prepare_recon_all.py for constructing a FreeSurfer recon-all pipeline. Both scripts automate logic registration, process directory creation, and pipeline assembly using the neuroanalyst framework.

---
+++ prepare_dipy_preprocessing.py
@@ -0,0 +1,141 @@
+# %% [markdown]
+# # DWI pre-processing pipeline
+# 
+# ## Processes
+# 1. dipy_denoise_mppca
+# 2. dipy_remove_gibbs_ringing
+# 3. dipy_correct_distortions_and_motion
+# 4. n4_bias_field_correction
+# 5. dipy_brain_mask
+
+# %%
+from src.neuroanalyst.models.recipe.core import (
+    create_logic_from_recipe,
+    create_process_dir_from_recipe,
+    construct_pipeline_from_recipe,
+    save_recipe_to_yaml,
+    get_recipe_yaml_path,
+    LogicRecipe,
+    ProcessDirRecipe,
+    ProcessExecInStepRecipe,
+    PipelineStepRecipe,
+    PipelineRecipe,
+    )
+from src.neuroanalyst.models.process.logic.core import NeuProcessLogic
+from src.neuroanalyst.models.process.dir.core import NeuProcessDir
+from src.neuroanalyst.models.pipeline.core import NeuPipeline
+
+import os
+from pathlib import Path
+
+# %%
+# os.environ["NEUROANALYST_HOME"] = "/Volumes/csi/cmokashi/neuroanalyst/"
+
+USERNAME: str = "cmokashi"
+AUTHOR: str = "Chinmay Mokashi"
+PIPELINE_NAME: str = "dipy_preprocessing"
+PIPELINE_DESCRIPTION: str = "Performs basic preprocessing steps on DWI data using DIPY."
+
+# %% [markdown]
+# ## Prepare Logics
+
+# %%
+logic_names: list[str] = [
+    "dipy_denoise_mppca",
+    "dipy_remove_gibbs_ringing",
+    "dipy_correct_distortions_and_motion",
+    "n4_bias_field_correction",
+    "dipy_brain_mask",
+]
+
+logic_descriptions: list[str] = [
+    "Denoise DWI data using MP-PCA method from DIPY.",
+    "Remove Gibbs ringing artifacts from DWI data using DIPY.",
+    "Correct for eddy currents, susceptibility distortions, and motion in DWI data using DIPY.",
+    "Apply N4 bias field correction to DWI data.",
+    "Generate brain mask from DWI data using DIPY.",
+]
+
+# %%
+sample_functions_root: Path = Path("src/neuroanalyst/models/process/logic/code/python/samples")
+
+function_paths: list[str] = [sample_functions_root / f"{func_name}.py" for func_name in logic_names]
+
+for logic_name, function_path in zip(logic_names, function_paths):
+    logic_recipe: LogicRecipe = LogicRecipe(
+        path=str(function_path),
+        username=USERNAME,
+        author=AUTHOR,
+    )
+    save_recipe_to_yaml(logic_recipe, logic_name, username=USERNAME)
+    logic: NeuProcessLogic = create_logic_from_recipe(get_recipe_yaml_path(logic_name, "logic", username=USERNAME))
+    logic.register(overwrite=True)
+
+# %% [markdown]
+# ## Prepare Processes
+
+# %%
+process_config: dict = {
+    "bootstrap_method": "localimage",
+    "base_image": "/rsrch5/home/csi/cmokashi/neuroanalyst/cmokashi/apptainer/images/base/python_312_slim_amd64_git_apptainer.sif",
+    "language_packages": {"python": ["dipy", "nibabel", "numpy", "SimpleITK"]},
+    }
+
+process_ids: list[str] = []
+for logic_name in logic_names:
+    process_dir_recipe: ProcessDirRecipe = ProcessDirRecipe(
+        logic={"name": logic_name, "username": USERNAME},
+        config=process_config,
+    )
+    save_recipe_to_yaml(process_dir_recipe, logic_name, username=USERNAME)
+    process_dir: NeuProcessDir = create_process_dir_from_recipe(get_recipe_yaml_path(logic_name, "process", username=USERNAME))
+    try:
+        process_dir.generate()
+        print(f"Process directory for {logic_name} created at {process_dir.working_dir}")
+        process_ids.append(process_dir.process_id)
+    except Exception as e:
+        print(f"Error creating process directory for {logic_name}: {e}")
+
+# %% [markdown]
+# ## Construct Pipeline
+
+# %%
+pipeline_recipe_config: dict = {
+    "username": USERNAME,
+    "author": AUTHOR,
+    "data": "/rsrch5/home/csi/cmokashi/neuroanalyst/cmokashi/datasets/ds004884-1.0.2/",
+    "name": PIPELINE_NAME,
+    "description": PIPELINE_DESCRIPTION,
+    "auto_link": True,
+    "cost": 8,
+    "steps": [
+        {
+            "name": logic_names[i],
+            "description": logic_descriptions[i],
+            "processes": [
+                {
+                    "process_id": process_ids[i],
+                    "input_bids_filters": {
+                        "datatype": "dwi",
+                        "suffix": "dwi",
+                        "extension": ".nii.gz",
+                    } if i == 0 else {},
+                }
+            ]
+        } for i in range(len(logic_names))
+    ]
+}
+pipeline_recipe: PipelineRecipe = PipelineRecipe(**pipeline_recipe_config)
+pipeline: NeuPipeline = construct_pipeline_from_recipe(save_recipe_to_yaml(pipeline_recipe, PIPELINE_NAME, username=USERNAME))
+
+# %%
+def get_build_script_commands() -> str:
+    commands: list[str] = []
+    for process_id in process_ids:
+        commands.append(f"python build_process.py {process_id} {USERNAME}")
+    return " & ".join(commands)
+
+print(get_build_script_commands())
+print(f"Pipeline '{pipeline.pipeline_id}' created.")
+print(pipeline)
+
---
+++ prepare_recon_all.py
@@ -0,0 +1,146 @@
+# %% [markdown]
+# # FreeSurfer recon-all pipeline via recipe-building
+# ## Steps
+# 1. Create and register Logics
+#   - autorecon1
+#   - autorecon2
+#   - autorecon3
+# 2. Create ProcessDirs and build images
+# 3. Construct Pipeline
+
+# %%
+from src.neuroanalyst.models.recipe.core import (
+    create_logic_from_recipe,
+    create_process_dir_from_recipe,
+    construct_pipeline_from_recipe,
+    save_recipe_to_yaml,
+    get_recipe_yaml_path,
+    LogicRecipe,
+    ProcessDirRecipe,
+    ProcessExecInStepRecipe,
+    PipelineStepRecipe,
+    PipelineRecipe,
+    )
+from src.neuroanalyst.models.process.logic.core import NeuProcessLogic
+from src.neuroanalyst.models.process.dir.core import NeuProcessDir
+from src.neuroanalyst.models.pipeline.core import NeuPipeline
+
+import os
+from pathlib import Path
+
+# %%
+# Set NEUROANALYST_HOME to the HPC installation
+# os.environ["NEUROANALYST_HOME"] = "/Volumes/csi/cmokashi/neuroanalyst/"
+
+# %%
+
+USERNAME: str = "cmokashi"
+AUTHOR: str = "Chinmay Mokashi"
+PIPELINE_NAME: str = "recon_all"
+PIPELINE_DESCRIPTION: str = "Executes the recon-all pipeline for preprocessing and cortical reconstruction in FreeSurfer."
+
+EXTRA_BIND_PATHS: dict = {"/risapps/rhel8/freesurfer/7.4.1": "/risapps/rhel8/freesurfer/7.4.1"}
+EXTRA_ENVIRONMENT_VARIABLES: dict = {"FREESURFER_HOME": "/risapps/rhel8/freesurfer/7.4.1"}
+
+# %% [markdown]
+# ## Prepare Logics
+
+# %%
+logic_names: list[str] = [
+    "autorecon1",
+    "autorecon2",
+    "autorecon3",
+]
+
+logic_descriptions: list[str] = [
+    "Executes the autorecon1 step of FreeSurfer's recon-all pipeline which includes motion correction, intensity normalization, and skull stripping.",
+    "Executes the autorecon2 step of FreeSurfer's recon-all pipeline which includes white matter segmentation, surface extraction, and topology correction.",
+    "Executes the autorecon3 step of FreeSurfer's recon-all pipeline which includes cortical parcellation and surface-based registration.",
+]
+
+sample_functions_root: Path = Path("src/neuroanalyst/models/process/logic/code/python/samples")
+
+function_paths: list[str] = [sample_functions_root / f"{func_name}.py" for func_name in logic_names]
+
+for logic_name, function_path in zip(logic_names, function_paths):
+    logic_recipe: LogicRecipe = LogicRecipe(
+        path=str(function_path),
+        username=USERNAME,
+        author=AUTHOR,
+    )
+    save_recipe_to_yaml(logic_recipe, logic_name, username=USERNAME)
+    logic: NeuProcessLogic = create_logic_from_recipe(get_recipe_yaml_path(logic_name, "logic", username=USERNAME))
+    logic.register(overwrite=True)
+
+# %% [markdown]
+# ## Prepare Processes
+
+# %%
+process_config: dict = {
+    "environment_variables": ["FREESURFER_HOME"],
+    "bind_paths": ["/risapps/rhel8/freesurfer/7.4.1"],
+    "command_flags": ["--fakeroot"],
+    "bootstrap_method": "localimage",
+    "base_image": "/rsrch5/home/csi/cmokashi/neuroanalyst/cmokashi/apptainer/images/base/for_freesurfer.sif",
+    }
+process_ids: list[str] = []
+for logic_name in logic_names:
+    process_dir_recipe: ProcessDirRecipe = ProcessDirRecipe(
+        logic={"name": logic_name, "username": USERNAME},
+        config=process_config,
+    )
+    save_recipe_to_yaml(process_dir_recipe, logic_name, username=USERNAME)
+    process_dir: NeuProcessDir = create_process_dir_from_recipe(get_recipe_yaml_path(logic_name, "process", username=USERNAME))
+    try:
+        process_dir.generate()
+        print(f"Process directory for {logic_name} created at {process_dir.working_dir}")
+        process_ids.append(process_dir.process_id)
+    except Exception as e:
+        print(f"Error creating process directory for {logic_name}: {e}")
+
+# %% [markdown]
+# ## Construct Pipeline
+
+# %%
+pipeline_recipe_config: dict = {
+    "username": USERNAME,
+    "author": AUTHOR,
+    "data": "/rsrch5/home/csi/cmokashi/neuroanalyst/cmokashi/datasets/ds004884-1.0.2/",
+    "name": PIPELINE_NAME,
+    "description": PIPELINE_DESCRIPTION,
+    "auto_link": True,
+    "cost": 10,
+    "steps": [
+        {
+            "name": logic_names[i],
+            "description": logic_descriptions[i],
+            "processes": [
+                {
+                    "process_id": process_ids[i],
+                    "input_bids_filters": {
+                        "suffix": "T1w",
+                        "extension": ".nii.gz",
+                        "datatype": "anat",
+                        } if i == 0 else {},
+                    "extra_bind_paths": EXTRA_BIND_PATHS,
+                    "extra_environment_variables": EXTRA_ENVIRONMENT_VARIABLES
+                }
+            ]
+        } for i in range(len(logic_names))
+    ]
+}
+pipeline_recipe: PipelineRecipe = PipelineRecipe(**pipeline_recipe_config)
+pipeline: NeuPipeline = construct_pipeline_from_recipe(save_recipe_to_yaml(pipeline_recipe, PIPELINE_NAME, username=USERNAME))
+
+# %%
+def get_build_script_commands() -> str:
+    commands: list[str] = []
+    for process_id in process_ids:
+        commands.append(f"python build_process.py {process_id} {USERNAME}")
+    return " & ".join(commands)
+
+print(get_build_script_commands())
+print(f"Pipeline '{pipeline.pipeline_id}' created.")
+print(pipeline)
+
+
