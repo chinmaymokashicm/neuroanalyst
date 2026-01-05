8d50981d0c4ffa2683187d2fcdee9ac58c34d7be
Mokashi,Chinmay <CMokashi@mdanderson.org>
2026-01-05 00:08:32 -0600

    Fix PROJECT_ROOT path to include 'src' directory
    Appends the 'src' directory to the PROJECT_ROOT path to ensure correct module resolution when modifying sys.path.

--- src/neuroanalyst/models/process/logic/run.py
+++ src/neuroanalyst/models/process/logic/run.py
@@ -4,7 +4,7 @@ Test or dry run a function or a Logic before registering and without integrating
 import sys
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).resolve().parents[5]
+PROJECT_ROOT = Path(__file__).resolve().parents[5] / "src"
 sys.path.insert(0, str(PROJECT_ROOT))
 
 from rich.console import Console
